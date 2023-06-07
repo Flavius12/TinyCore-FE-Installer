390216f148681dbea4c017ce848bfaddd4c8a37b
Flavius12 <flavius.c.1999@gmail.com>
2023-06-07 16:56:59 +0200

    Setup pages navigation completed and partitioning WIP

---
+++ test.py
@@ -0,0 +1,18 @@
+import parted
+import os
+import sys
+
+euid = os.geteuid()
+if euid != 0:
+    print("Script not started as root. Running sudo..")
+    args = ['sudo', sys.executable] + sys.argv + [os.environ]
+    # the next line replaces the currently-running process with the sudo
+    os.execlpe('sudo', *args)
+
+print(parted.getAllDevices())
+device = parted.getDevice("/dev/sdb")
+disk = parted.freshDisk(device, "msdos")
+#disk = parted.newDisk(device)
+print(device.length * device.sectorSize)
+print(disk.maxPrimaryPartitionCount)
+print()
